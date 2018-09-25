d95c0f7fadf3542c1bbb8d2baf15f7f939efe0aa
Thiago Mallon <jamesmallondev@gmail.com>
2018-09-25 00:15:13 -0700

    Adding doc tags to the User model

--- app/User.php
+++ app/User.php
@@ -1,11 +1,32 @@
 <?php
+/**
+ * PHP Version 7.2
+ *
+ * @category Models
+ * @package  App
+ * @author   Thiago Mallon <thiagomallon@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://www.linkedin.com/in/thiago-mallon/
+ */
 
+/**
+ * File namespace
+ */
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * Class User
+ *
+ * @category Models
+ * @package  App
+ * @author   Thiago Mallon <thiagomallon@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://www.linkedin.com/in/thiago-mallon/
+ */
 class User extends Authenticatable
 {
     use Notifiable;
