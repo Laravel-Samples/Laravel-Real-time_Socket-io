83c59270af5e6dcfd6178f85a13823a13821575e
Thiago Mallon <jamesmallondev@gmail.com>
2018-09-25 12:01:04 -0700

    Changing the resource folder structure

--- resources/js/dashboard/app.js
+++ resources/js/dashboard/app.js
@@ -1,11 +1,11 @@
 
 /**
  * First we will load all of this project's JavaScript dependencies which
- * includes React and other helpers. It's a great starting point while
- * building robust, powerful web applications using React + Laravel.
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require('./../bootstrap');
 
 window.Vue = require('vue');
 
@@ -14,8 +14,6 @@ window.Vue = require('vue');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
-Vue.component('public-message', require('./components/PublicMessage.vue'));
 
 const app = new Vue({
     el: '#app'
