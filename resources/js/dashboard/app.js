f00ee1d3b24098155419973f5ed2b10e24825274
Thiago Mallon <jamesmallondev@gmail.com>
2018-09-26 01:36:38 -0700

    Adding the UpdateHomeMessage.vue component, to deal with Home Message sending

--- resources/js/dashboard/app.js
+++ resources/js/dashboard/app.js
@@ -14,6 +14,7 @@ window.Vue = require('vue');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+Vue.component('home-message-block', require('./components/UpdateHomeMessage.vue'));
 
 const app = new Vue({
     el: '#app'
