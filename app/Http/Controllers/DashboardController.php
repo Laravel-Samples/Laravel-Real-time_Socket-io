e27d208fef4d048c3cca84336ebe948c434552a5
Thiago Mallon <jamesmallondev@gmail.com>
2018-09-26 01:27:44 -0700

    Fixing indentation

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,10 @@
  * PHP Version 7.2
  *
  * @category Controllers
- * @package App\Http\Controllers
- * @author  Thiago Mallon <thiagomallon@gmail.com>
- * @license MIT https://opensource.org/licenses/MIT
- * @link    https://www.linkedin.com/in/thiago-mallon/
+ * @package  App\Http\Controllers
+ * @author   Thiago Mallon <thiagomallon@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://www.linkedin.com/in/thiago-mallon/
  */
 
 /**
@@ -17,15 +17,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\HomeMessageRequest; 
+use App\Events\PublicMessageSent;   
 
 /**
  * Class DashboardController
  *
  * @category Controllers
- * @package App\Http\Controllers
- * @author  Thiago Mallon <thiagomallon@gmail.com>
- * @license MIT https://opensource.org/licenses/MIT
- * @link    https://www.linkedin.com/in/thiago-mallon/
+ * @package  App\Http\Controllers
+ * @author   Thiago Mallon <thiagomallon@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://www.linkedin.com/in/thiago-mallon/
  */
 class DashboardController extends Controller
 {
@@ -48,5 +50,22 @@ class DashboardController extends Controller
     public function index(): object
     {
         return view('dashboard.home');
+    }
+    
+    /**
+     * Public method sendPublicMessage - Method dispatch the broadcasting event
+     *
+     * @param \App\Http\Requests\HomeMessageRequest $request The request data
+     *
+     * @method string sendPublicMessage Method dispatch the broadcasting event
+     * @return string 
+     */
+    public function sendPublicMessage(HomeMessageRequest $request): object
+    {
+        auth()->user()->homeMessages()
+            ->create(['message' => $request->message]); // save the message
+            // dispatch the event 
+            PublicMessageSent::dispatch($request->message);
+            return response(request('message'), 200); // return in json format
     }
 }
