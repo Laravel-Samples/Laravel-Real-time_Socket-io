8db05cec5e8d6b98483b369c25e3eaff0c1e72b2
Thiago Mallon <jamesmallondev@gmail.com>
2018-09-26 22:39:11 -0700

    adding the admin-lte to the dashboard page

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -4,21 +4,14 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header"><h3>Dashboard</h3></div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <h3>Hi {{ auth()->user()->username}}, write the home message bellow.</h3>
-                    <home-message-block></home-message-block>
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
 
+            <h3>Hi {{ auth()->user()->username}}, write the home message bellow.</h3>
+            <home-message-block></home-message-block>
         </div>
     </div>
 </div>
