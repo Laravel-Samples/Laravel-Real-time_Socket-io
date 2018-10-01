61248a263950564db54780610ffb9e7c399a5abc
Thiago Mallon <jamesmallondev@gmail.com>
2018-09-30 17:02:02 -0700

    Updating page and error messages layout

--- resources/js/dashboard/components/UpdateHomeMessage.vue
+++ resources/js/dashboard/components/UpdateHomeMessage.vue
@@ -1,25 +1,33 @@
 <template>
     <div>
-        <input type="text" v-model="newMessage" @keydown.enter="save" class="form-control" :class="inputRes" autofocus>
-         <div :class="feedbackRes" v-html="feedbackBox"></div>
-        <button type="button" class="btn btn-primary mt-4" @click="save">Go!</button>
+        <div class="form-row">
+            <div class="col">
+                <label for="transfer-username">Home Message</label>
+                <input type="text" v-model="newMessage" @keydown.enter="save"
+                class="form-control" v-bind:class="{'is-invalid': invalidRes}"
+                placeholder="Write here your home message..." autofocus>
+                <div :class="{'invalid-feedback': invalidRes}" v-html="feedbackBox"></div>
+            </div>
+        </div>
+         <button type="button" class="btn btn-primary mt-4" @click="save">
+             <strong>Send!</strong>
+         </button>
     </div>
 </template>
 
 <script>
     export default {
-        data() { 
+        data() {
             return {
                 newMessage: '',
                 feedbackBox: '',
-                feedbackRes: '',
-                inputRes: ''
+                invalidRes: false,
             }
         },
-        methods: {            
+        methods: {
             save() {
                 if (this.newMessage.length) {
-                    axios.post(`/ajax/send-message/`, {message: this.newMessage })
+                    axios.post(`/ajax/send-message`, {message: this.newMessage })
                      .catch(function (error) {
                             return error.response;
                       }).then(this.setResponse);
@@ -29,15 +37,13 @@
             },
             setResponse(response) {
                 if (response.status != 200) {
-                    this.feedbackRes = "invalid-feedback";
-                    this.inputRes = "is-invalid";
-                    this.feedbackBox = response.data.errors.message[0];
+                    this.invalidRes = true;
+                    this.feedbackBox = response.data.errors.message[0];                    
                 } else {
-                    this.feedbackRes = "";
-                    this.inputRes = "";
+                    this.invalidRes = false;
                     this.feedbackBox = "Your message was sent!";
                 }
             }
-        }        
+        }
     };
 </script>
